b0eeaa45f2513163d9d9971724a11cd529421c8c
Andrey <andreykoloskov.ru@gmail.com>
2018-08-21 08:16:58 +0300

    boost

--- multithread_programs/sfera-mail-mt/network/echo/boost/echo.cpp
+++ multithread_programs/sfera-mail-mt/network/echo/boost/echo.cpp
@@ -51,8 +51,7 @@ private:
 public:
     server(boost::asio::io_service &io_service, unsigned short port):
             io_service_(io_service),
-            acceptor_(io_service, tcp::endpoint(
-                    (const boost::asio::ip::address&) tcp::v4, port)) {
+            acceptor_(io_service, tcp::endpoint(tcp::v4(), port)) {
         session *new_session = new session(io_service_);
         acceptor_.async_accept(new_session->socket(),
                 boost::bind(&server::handle_accept, this, new_session,
